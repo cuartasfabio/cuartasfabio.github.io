44516a8e05b4d3681cb6e0316562d879f96f5d6d
cuartasfabio <57012072+cuartasfabio@users.noreply.github.com>
2020-11-29 14:41:29 +0100

    Add files via upload

---
+++ CalculadoraBasica.js
@@ -0,0 +1,42 @@
+"use strict"
+class Calculadora {
+
+    constructor(){
+        this.memory = 0;
+    }
+
+    insertarToken(t){
+        document.getElementById("texto").value += t;
+    }
+
+    mrc(){
+        if(this.memory != 0){
+            document.getElementById("texto").value = this.memory;
+        } else if(this.memory == parseInt(document.getElementById("texto").value)){
+            this.memory = 0;
+        }
+    }
+
+    mMenos(){
+        this.memory += parseInt(document.getElementById("texto").value)
+    }
+
+    mMas(){
+        this.memory += parseInt(document.getElementById("texto").value)
+    }
+
+    borrar(){
+        document.getElementById("texto").value = "";
+    }
+
+    igual(){
+        var x;
+        x = document.getElementById("texto").value;
+        try { 
+            document.getElementById("texto").value = eval(x);
+        }
+        catch(err) {
+            document.getElementById("texto").value = "Error de sintaxis!";
+        }  
+    }
+}
