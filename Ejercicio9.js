7e2ac20d00672ab1693e1c686280944617bed260
cuartasfabio <57012072+cuartasfabio@users.noreply.github.com>
2021-06-21 10:58:26 +0200

    Add files via upload

---
+++ Ejercicio9.js
@@ -0,0 +1,21 @@
+"use strict";
+class Parser {
+
+    constructor(){}
+
+    cargarDatos(){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this);
+            }
+        };
+        xhttp.open("GET", "http://docs.gijon.es/sw/busgijon.asmx/Paradas", true);
+        xhttp.send();
+    }
+
+    verXML(){
+        this.cargarDatos();
+    }
+}
+var parser = new Parser();
